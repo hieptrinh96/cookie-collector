6b413b8ea51e7852c0e8180e5781e3a0078f6b68
HiepTrinh <xmikecolx@gmail.com>
2022-11-07 12:03:10 -0600

    complete cookie details, working on create cookie

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('cookies/', views.cookies_index, name='cookies_index'),
-  path('cookies/<int:cookie_id>/', views.cookies_detail, name='cookies_detail')
-
+  path('cookies/<int:cookie_id>/', views.cookies_detail, name='cookies_detail'),
+  path('cookies/create/', views.CookieCreate.as_view(), name='cookies_create')
 ]
