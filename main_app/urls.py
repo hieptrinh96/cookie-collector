9b684ce5ce9637c70a7607b90fa22c4f3d186108
HiepTrinh <xmikecolx@gmail.com>
2022-11-07 08:06:02 -0600

    add in cookie model

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('cookies/', views.cookies_index, name='cookies_index'),
+  path('cookies/<int:cookie_id>/', views.cookies_detail, name='cookies_detail')
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from .models import Cookie
 
-from django.http import HttpResponse
 # Create your views here.
 
 # Add the Cat class & list and view function below the imports
@@ -10,14 +10,6 @@ class Cookie:  # Note that parens are optional if not inheriting from another cl
     self.flavor = flavor
     self.description = description
     self.quantity = quantity
-    
-
-cookies = [
-  Cookie('Milk Chocolate Chip', 'Milk Chocolate Chip', 'the classic- thinkc,soft, and packed with milk chocolate chips', 3),
-  Cookie('Peanut Butter', 'Peanut Butter', 'A blast of flavors including peanut butter, nougat, vanilla bean, and milk chocolate', 2),
-  Cookie('Blue Monster', 'brown sugar and chips ahoy', 'a cookie popping with brown sugar, semi-sweet chocolate chips, and chips ahoy', 4),
-  Cookie('Almond Coconut', 'coconut and fudge', 'a rich chocolatey cookie topped with a spread of sweet coconut, melty milk chocolate, and crunchy almonds', 6)
-]
 
 def home(request):
   return render(request, 'home.html')
@@ -26,4 +18,9 @@ def about(request):
   return render(request, 'about.html')
 
 def cookies_index(request):
+  cookies = Cookie.objects.all()
   return render(request, 'cookies/index.html', {'cookies': cookies})
+
+def cookies_detail(request, cookie_id):
+  cookie = Cookie.objects.get(id=cookie_id)
+  return render(request, 'cookies/detail.html', {'cookie': cookie})
